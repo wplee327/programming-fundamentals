f61adaf64c931a64f1e6fd75859c83da4c048019
wplee327 <wplee.327@gmail.com>
2022-05-01 20:33:25 -0400

    Add comments to Calculator.vb

--- Calculator.vb
+++ Calculator.vb
@@ -1,7 +1,7 @@
-Namespace Global.Mathematics
-    Class Calculator
-        Function Addition(a As Integer, b As Integer) As Integer
-            return a + b
+Namespace Global.Mathematics ' Create a namespace named Mathematics to contain our code and make it Global so it is visible to the rest of the program.
+    Class Calculator ' Create a class named Calculator to contain the methods available in this class.
+        Function Addition(a As Integer, b As Integer) As Integer ' Create a function named Addition that accepts two integers and outputs one integer.
+            return a + b ' Perform addition on the two input numbers and output the answer.
         End Function
 
         Function Subtraction(a As Integer, b As Integer) As Integer
